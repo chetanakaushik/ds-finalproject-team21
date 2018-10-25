3fb64f49e501860b3fdc08b920c5bb056f45e6ea
Vasu Patel <vasupatel@outlook.com>
2018-10-25 16:29:26 -0400

    Sensor fetch function

--- app/model/Sensor.php
+++ app/model/Sensor.php
@@ -15,5 +15,22 @@ class Sensor{
     $this->totalLifeExpectancy = $row['totalLifeExpectancyHours'];
   }
 
+  public static function fetchAllSensors() {
+    // 1. Connect to the database
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+    // 2. Prepare the query
+    $sql = 'SELECT * FROM Sensors';
+    $statement = $db->prepare($sql);
+    // 3. Run the query
+    $success = $statement->execute();
+    // 4. Handle the results
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+      $theSensor =  new Sensor($row);
+      array_push($arr, $theSensor);
+    }
+    return $arr;
+  }
+
 
 }
