2725a2d734b1cee69551b87856c08716372a9f7e
Vasu Patel <vspatel@iu.edu>
2018-11-02 16:31:11 -0400

    added notes all fetch

---
+++ public/api/notes_all.php
@@ -0,0 +1,14 @@
+<?php
+ require '../../app/common.php';
+
+ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+   require 'notesPost.php';
+   exit;
+ }
+
+$notes = ClientNotes::fetchAllNotes();
+
+$json = json_encode($notes, JSON_PRETTY_PRINT);
+
+header('Content-Type: application/json');
+echo $json;
